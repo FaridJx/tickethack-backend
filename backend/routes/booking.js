1f47a6d29b87d0fc8b7f02af9f4913fc53bd454d
Farid <farid.adinani@hotmail.com>
2023-09-19 18:46:36 +0200

    route trips sans la date

---
+++ backend/routes/booking.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+const moment = require('moment')
+
+
+router.post('/', (req, res) => {
+    
+})
+
+
+
+
+
+
+module.exports = router
---
+++ backend/routes/trips.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+const moment = require('moment')
+const Trip = require('../models/trips')
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+  Trip.find().then(data => {
+    res.json({trips: data})
+  })
+});
+
+// router.get('/selec', (req, res) => {
+//     Trip.findOne({departure: req.body.departure}).then(data => {
+//         res.json({departure: data})
+//     })
+// })
+
+router.post('/selec', (req, res) => {
+    if(!req.body.departure || !req.body.arrival){
+        res.json({result: false, error: 'Il manque un élément'})
+        return
+    }
+
+    Trip.find({departure: req.body.departure, arrival: req.body.arrival}).then(data => {
+        // data.date = moment(data.date).format('YYYY');
+        if(data && data.length > 0){
+            data.forEach(trip => {
+                trip.date = moment(trip.date).format('YYYY');
+            });
+            res.json({data})
+        } else{
+            res.json({result: false, error: `Ce trajet n'est pas disponible`})
+        }
+    })
+
+})
+
+
+
+module.exports = router;
