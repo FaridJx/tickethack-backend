0ba41f36d777a92c4eb74d5047a7ad6d2d03b0e6
Farid <farid.adinani@hotmail.com>
2023-09-20 12:44:48 +0200

    modif app.use(cors())

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var bookingRouter = require('./routes/booking');
 
 var app = express();
 const cors = require('cors')
+app.use(cors())
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/trips', tripsRouter);
 app.use('/booking', bookingRouter);
-app.use(cors())
 
 
 module.exports = app;
