aef3df1823024ef122f9b957500b86e044183b45
Farid <farid.adinani@hotmail.com>
2023-09-20 11:57:14 +0200

    fichier .env

--- models/connexion.js
+++ models/connexion.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://admin:boubou92@cluster0.xnpa1t3.mongodb.net/trips';
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
