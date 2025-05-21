802a1ab3da00b5daaff4a6b9402b0c3cf2c07d11
Farid <farid.adinani@hotmail.com>
2025-05-21 23:42:11 +0200

    demarrage clean code

--- routes/trips.js
+++ routes/trips.js
@@ -4,18 +4,12 @@ const moment = require('moment')
 moment.locale('fr')
 const Trip = require('../models/trips')
 
-/* GET users listing. */
 router.get('/', (req, res, next) => {
   Trip.find().then(data => {
     res.json({trips: data})
   })
 });
 
-// router.get('/selec', (req, res) => {
-//     Trip.findOne({departure: req.body.departure}).then(data => {
-//         res.json({departure: data})
-//     })
-// })
 
 router.post('/selec', (req, res) => {
     if(!req.body.departure || !req.body.arrival || !req.body.date ){
@@ -30,15 +24,6 @@ router.post('/selec', (req, res) => {
     nextDay.setDate(nextDay.getDate() + 1);
 
     Trip.find({departure: req.body.departure, arrival: req.body.arrival, date: { $gte: inputDate, $lt: nextDay}}).then(data => {
-        // console.log(data.date = moment(data.date).format('YYYY-MM-DD'))
-        // if(data && data.length > 0){
-        //     data.forEach(trip => {
-        //         trip.date = moment(trip.date).format('YYYY');
-        //     });
-        //     res.json({data})
-        // } else{
-        //     res.json({result: false, error: `Ce trajet n'est pas disponible`})
-        // }
         if (data && data.length > 0) {
             // Formatez les dates dans le format souhaité (par exemple, 'YYYY-MM-DD')
             const formattedData = data.map(trip => ({
