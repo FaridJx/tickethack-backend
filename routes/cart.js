85ea322f216b7c00a6dd396a2d40eac3202dc4d9
Farid <farid.adinani@hotmail.com>
2025-05-22 23:42:16 +0200

    ajout de commentaire

--- routes/cart.js
+++ routes/cart.js
@@ -2,12 +2,15 @@ var express = require('express');
 var router = express.Router();
 const Cart = require('../models/cart')
 
+// Recupère tous les voyages enregistrés 
 
 router.get('/', (req, res, next) => {
     Cart.find().then(data => {
       res.json(data)
     })
   });
+
+// Créer un nouveau voyage dans la page panier
 
 router.post('/new', (req, res) => {
     if(!req.body.departure || !req.body.arrival || !req.body.date || !req.body.price ){
@@ -35,6 +38,8 @@ router.post('/new', (req, res) => {
 })
 
 
+// Supprime un trajet spécifique
+
 router.delete('/delete', (req, res) => {
     if(!req.body.departure || !req.body.arrival || !req.body.date){
       res.json({result: false, error: 'Il manque un élément'})
@@ -51,6 +56,8 @@ router.delete('/delete', (req, res) => {
      })
 })
 
+
+// Supprime tous les trajets
 
 router.delete('/deleteAll', (req, res) => {
     Cart.deleteMany({})
